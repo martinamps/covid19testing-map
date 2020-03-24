a96c0648adbd4988cb2debabdcccfcac3fbccc55
Martin Amps <mamps@twilio.com>
2020-03-24 00:24:38 -0500

    add optimized tiles flag to mapbox

--- src/Components/Map.tsx
+++ src/Components/Map.tsx
@@ -46,7 +46,7 @@ export const Map = (props: MapProps) => {
         onViewStateChange={setViewState}
         ref={mapRef}
         style={{ zIndex: 50, position: "relative" }}
-        mapStyle="mapbox://styles/mapbox/streets-v11"
+        mapStyle="mapbox://styles/mapbox/streets-v11?optimize=true"
         mapboxApiAccessToken={MAPBOX_TOKEN}
       >
         <Navigation>
